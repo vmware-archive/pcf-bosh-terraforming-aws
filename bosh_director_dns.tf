93e7ba9379fa5d81ad1fe1659cb8fafadf072a33
Derek Richard <drichard@pivotal.io>
2017-01-30 15:21:06 -0800

    Fix director dns record
    - Previously it was a cname, but needs to be an A record.
    
    [#138640199]

--- bosh_director_dns.tf
+++ bosh_director_dns.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "director" {
   zone_id = "${aws_route53_zone.pcf_zone.id}"
   name    = "director.${var.env_name}.${var.dns_suffix}"
-  type    = "CNAME"
+  type    = "A"
   ttl     = 300
 
   records = ["${aws_eip.director_external_ip.public_ip}"]
