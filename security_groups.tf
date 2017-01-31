4beec04556ee484c74e051f0f624acde83789040
Derek Richard <drichard@pivotal.io>
2017-01-31 08:46:02 -0800

    Allow egress from bosh_security group
    [#138356735]

--- security_groups.tf
+++ security_groups.tf
@@ -48,6 +48,13 @@ resource "aws_security_group" "bosh_security_group" {
     to_port     = 8443
   }
 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     Name = "${var.env_name}-bosh-security-group"
   }
